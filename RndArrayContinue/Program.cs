3e6d7f19af611d9230d85a63e846762d0f48c6be
asengin <worksmail2017@gmail.com>
2022-02-12 03:29:15 +0300

    С передачей аргументов

--- RndArrayContinue/Program.cs
+++ RndArrayContinue/Program.cs
@@ -8,52 +8,77 @@ namespace RndArrayContinue
 {
     class Program
     {
-        static int[] arrayRnd;
-        static void Main(string[] args)
+        /*
+         * Реализованы задачи продолжения с передачей результатов от задачи к задаче.
+         * В задаче taskRndArray осуществляется формирование массива случайных чисел заданной пользователем размерности
+         * Далее продолжают работать задачи по нахождению суммы, максимального и минимального элемента массива.
+         * Для мин и макс применил готовый метод Min Max для массивов и лямбда выражения.
+         * Дожидаемся работы всех задач через Task.WaitAll.
+         * Выводим результат через отдельную задачу.
+         */
+         static void Main(string[] args)
         {
             Console.Write("Введите размер массива: ");
             int arraySize = Convert.ToInt32(Console.ReadLine());
 
-            Task task1 = new Task(() => RndArray(arraySize));
-            Action<Task> actionSum = new Action<Task>(SumArray);
-            Task task2 = task1.ContinueWith(actionSum);
-            Action<Task> actionMinMax = new Action<Task>(MinMaxArray);
-            Task task3 = task2.ContinueWith(actionMinMax);
+            #region Вариант с передачей параметров от задачи к задаче. Доработаны методы
+            Func<object, int[]> funcRndArray = new Func<object, int[]>(RndArray);
+            Task<int[]> taskRndArray = new Task<int[]>(funcRndArray, arraySize);
 
-            task1.Start();
-            //RndArray(arraySize);
-            //SumArray(arrayRnd);
-            //MinMaxArray(arrayRnd);
+            Func<Task<int[]>, int> funcSumArray = new Func<Task<int[]>, int>(SumArray);
+            Task<int> taskSumArray = taskRndArray.ContinueWith<int>(funcSumArray);
 
+            Task<int> taskMaxInArray = taskRndArray.ContinueWith<int>((Task<int[]> task) => task.Result.Max());
+            Task<int> taskMinInArray = taskRndArray.ContinueWith<int>((Task<int[]> task) => task.Result.Min());
+
+            Task taskPrint = new Task(() => PrintArrayInfo(taskRndArray, taskSumArray, taskMaxInArray, taskMinInArray));
+
+            taskRndArray.Start();
+            Task.WaitAll(taskSumArray, taskMaxInArray, taskMinInArray);
+
+            taskPrint.Start();
+            #endregion
             Console.ReadKey();
         }
 
-        static void RndArray(int arraySize) //Метод формирует массиив случайных чисел в диапазоне от -100 до 100
+        static int[] RndArray(object size) //Метод формирует массиив случайных чисел в диапазоне от -100 до 100
         {
-            arrayRnd = new int[arraySize];
+            int arraySize = (int)size;
+            int[] arrayRnd = new int[arraySize];
             Random rnd = new Random();
-            Console.Write("Элементы массива: ");
+            //Console.Write("Элементы массива: ");
             for (int i = 0; i < arraySize; i++)
             {
                 arrayRnd[i] = rnd.Next(-100, 100);
-                Console.Write($"{arrayRnd[i]} ");
+                //Console.Write($"{arrayRnd[i]} ");
             }
-            Console.WriteLine();
+            //Console.WriteLine();
+            return arrayRnd;
         }
 
-        static void SumArray(Task task) //Метод принимает массив и возвращает сумму элементов
+        static int SumArray(Task<int[]> task) //Метод принимает таск-массив, вычисляет и возвращает сумму элементов
         {
-
+            int[] arrayRnd = task.Result;
             int sum = 0;
             foreach (int k in arrayRnd)
                 sum += k;
-            Console.Write($"Сумма элементов: {sum}\n");
+            return sum;
         }
-
-        static void MinMaxArray(Task task)
+        static void PrintArrayInfo(Task<int[]> taskArray, Task<int> taskSum, Task<int> taskMax, Task<int> taskMin) //Метод принимает выполненные таски и выводит результаты
         {
-            Console.WriteLine($"Максимальный элемент массива: {arrayRnd.Max()}");
-            Console.WriteLine($"Минимальный элемент массива: {arrayRnd.Min()}");
+            int[] array = taskArray.Result;
+            int sum = taskSum.Result;
+            int max = taskMax.Result;
+            int min = taskMin.Result;
+
+            Console.Write("Элементы массива: ");
+            for (int i = 0; i < array.Count(); i++)
+                Console.Write($"{array[i]} ");
+
+            Console.WriteLine($"\nСумма элементов массива: {sum}\n" +
+                $"Максимальный элемент массива: {max}\n" +
+                $"Минимальный элемент массива: {min}");
         }
+             
     }
 }
